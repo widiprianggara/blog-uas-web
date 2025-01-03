8dd9d414f863440d585299e0e06bbc2aaa645e50
arya <aryadharmawasudewa@gmail.com>
2025-01-03 21:49:02 +0800

    Front End

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// script untuk responsive navbar
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuIcon = document.getElementById("menu-icon");
+  const menuList = document.getElementById("menu-list");
+
+  menuIcon.addEventListener("click", () => {
+      menuList.classList.toggle("hidden");
+  });
+});
