480bad253e6c8c1c3a730aa8e93e10e2c29ce8cc
arya <aryadharmawasudewa@gmail.com>
2025-01-06 07:03:32 +0800

    update js untuk tampilan

--- js/script.js
+++ js/script.js
@@ -8,3 +8,30 @@ document.addEventListener("DOMContentLoaded", () => {
       menuList.classList.toggle("hidden");
   });
 });
+
+// script untuk responsive navbar
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuIcon = document.getElementById("menu-icon");
+  const menuList = document.getElementById("menu-list");
+
+  menuIcon.addEventListener("click", () => {
+      menuList.classList.toggle("hidden");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const userIcon = document.getElementById("user-icon");
+  const dropdownMenu = document.getElementById("dropdown-menu");
+
+  userIcon.addEventListener("click", function () {
+    dropdownMenu.classList.toggle("active");
+  });
+
+  // Menutup dropdown jika pengguna mengklik di luar
+  document.addEventListener("click", function (event) {
+    if (!userIcon.contains(event.target)) {
+      dropdownMenu.classList.remove("active");
+    }
+  });
+});
