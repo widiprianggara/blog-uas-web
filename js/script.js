52507fb348bde108da986f067471bffaeaaaceed
widiprianggara <prianggarakadekwidi@gmail.com>
2025-01-05 22:42:34 +0800

    index-html

--- js/script.js
+++ js/script.js
@@ -8,3 +8,19 @@ document.addEventListener("DOMContentLoaded", () => {
       menuList.classList.toggle("hidden");
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const userIcon = document.getElementById("user-icon");
+  const dropdownMenu = document.getElementById("dropdown-menu");
+
+  userIcon.addEventListener("click", function () {
+    dropdownMenu.classList.toggle("active");
+  });
+
+  // Menutup dropdown jika pengguna mengklik di luar
+  document.addEventListener("click", function (event) {
+    if (!userIcon.contains(event.target)) {
+      dropdownMenu.classList.remove("active");
+    }
+  });
+});
